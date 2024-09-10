1dd3fbfeb6ef14a936b9d71391d3c00659ce8dc3
Equallizerr1 <samharradine1994@gmail.com>
2024-09-10 11:34:42 +0100

    added code to save to localStorage

--- src/components/NewHome.tsx
+++ src/components/NewHome.tsx
@@ -44,12 +44,31 @@ function NewHome() {
         })()
     }, [])
 
-    const addToList = (id: number) => {
-        if (artworkIds.includes(id)) {
-            return
-        }
-        artworkIds.push(id)
-        setArtworkIds(artworkIds)
+    // const addToList = (id: number) => {
+    //     if (artworkIds.includes(id)) {
+    //         return
+    //     }
+    //     artworkIds.push(id)
+    //     setArtworkIds(artworkIds)
+    // }
+
+    const addToList = (input: number) => {
+        let listArray = this.state.lists
+
+        listArray.push(input)
+
+        this.setState(
+            {
+                lists: listArray,
+                userInput: '',
+            },
+            () => {
+                window.localStorage.setItem(
+                    'savedList',
+                    JSON.stringify(this.state.lists)
+                )
+            }
+        )
     }
 
     return (
@@ -139,55 +158,50 @@ function NewHome() {
                 </React.Fragment>
                 <h1 className="text-3xl font-bold text-text underline">Home</h1>
             </div>
-            <div className="h-24 bg-primary">
-                <div className="bg-primary text-left">
-                    {artworks.map((artwork: ArtworkProps) => (
-                        <Card
-                            size="lg"
-                            sx={{ width: 640, margin: 1 }}
-                            key={artwork.id}
-                        >
-                            <CardOverflow>
-                                <AspectRatio
-                                    minHeight="120px"
-                                    maxHeight="800px"
-                                >
-                                    <img
-                                        src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
-                                        srcSet={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
-                                        loading="lazy"
-                                        alt={artwork.thumbnail.alt_text}
-                                    />
-                                </AspectRatio>
-                            </CardOverflow>
-                            <Typography level="title-lg">
-                                {artwork.title}
-                                <Typography level="body-sm">
-                                    {` ${artwork.date_display}`}
-                                </Typography>
-                            </Typography>
+            <div className="bg-primary text-left">
+                {artworks.map((artwork: ArtworkProps) => (
+                    <Card
+                        size="lg"
+                        sx={{ width: 640, margin: 1 }}
+                        key={artwork.id}
+                    >
+                        <CardOverflow>
+                            <AspectRatio minHeight="120px" maxHeight="800px">
+                                <img
+                                    src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
+                                    srcSet={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
+                                    loading="lazy"
+                                    alt={artwork.thumbnail.alt_text}
+                                />
+                            </AspectRatio>
+                        </CardOverflow>
+                        <Typography level="title-lg">
+                            {artwork.title}
                             <Typography level="body-sm">
-                                {artwork.artist_title}
+                                {` ${artwork.date_display}`}
                             </Typography>
-                            <IconButton
-                                aria-label="bookmark Bahamas Islands"
-                                variant="soft"
-                                color="neutral"
-                                size="sm"
-                                sx={{
-                                    position: 'absolute',
-                                    top: '0.875rem',
-                                    right: '0.5rem',
-                                }}
-                                onClick={() => {
-                                    addToList(artwork.id)
-                                }}
-                            >
-                                <BookmarkAdd />
-                            </IconButton>
-                        </Card>
-                    ))}
-                </div>
+                        </Typography>
+                        <Typography level="body-sm">
+                            {artwork.artist_title}
+                        </Typography>
+                        <IconButton
+                            aria-label="bookmark Bahamas Islands"
+                            variant="soft"
+                            color="neutral"
+                            size="sm"
+                            sx={{
+                                position: 'absolute',
+                                top: '0.875rem',
+                                right: '0.5rem',
+                            }}
+                            onClick={() => {
+                                addToList(artwork.id)
+                            }}
+                        >
+                            <BookmarkAdd />
+                        </IconButton>
+                    </Card>
+                ))}
             </div>
         </>
     )
